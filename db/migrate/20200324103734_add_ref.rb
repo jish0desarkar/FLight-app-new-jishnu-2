92d2b56044756655fd97a33f3b67722cd4ef5dd3
jish0desarkar <desarkarjishnu@gmail.com>
2020-03-24 18:49:42 +0530

    dix: migration

--- db/migrate/20200324103734_add_ref.rb
+++
@@ -1,6 +0,0 @@
-class AddRef < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :tickets, :passenger, null: false, foreign_key: true
-    add_reference :tickets, :flight, null: false, foreign_key: true
-  end
-end
